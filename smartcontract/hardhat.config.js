36ed3303f366bbb9cfad345342f47d56578a85cf
piya-ds <86950823+piya-ds@users.noreply.github.com>
2023-07-23 00:50:19 +0530

    first commit

--- smartcontract/hardhat.config.js
+++ smartcontract/hardhat.config.js
@@ -1,12 +1,16 @@
 //https://eth-ropsten.alchemyapi.io/v2/5awlhZNKv7E7JFTnxDKUYXwLgcoHlw29
 require('@nomiclabs/hardhat-waffle');
 
+require('dotenv').config();
+
+const { API_URL, PRIVATE_KEY } = process.env;
+
 module.exports = {
   solidity: '0.8.0',
   networks: {
-    ropsten: {
-      url: 'https://eth-ropsten.alchemyapi.io/v2/5awlhZNKv7E7JFTnxDKUYXwLgcoHlw29',
-      accounts: ['8c6c2d11086c93b0e92b2bedd7162ee28c7c746463e4c0b26ecbd86c181db0c8'],
+    MATIC_MUMBAI: {
+      url: 'API_URL',
+      accounts: ['PRIVATE_KEY'],
     },
   },
-};
+}; 
